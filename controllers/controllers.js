4cfa3752c9a959f443f321f9bc1cac2f04a5b06e
georgia <geaoneill@googlemail.com>
2024-10-15 16:33:06 +0100

    show working get article by id endpoint

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,9 +1,21 @@
-const { fetchTopics } = require("../models/models");
+const { fetchTopics, fetchArticleById } = require("../models/models");
 
 exports.getTopics = (request, response, next) => {
   fetchTopics()
     .then((topics) => {
       response.status(200).send({ topics });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getArticleById = (request, response, next) => {
+  const { article_id } = request.params;
+  
+  return fetchArticleById(article_id)
+    .then((article) => {
+      response.status(200).send({ article });
     })
     .catch((err) => {
       next(err);
--- models/models.js
+++ models/models.js
@@ -5,3 +5,17 @@ exports.fetchTopics = () => {
     return result.rows;
   });
 };
+
+exports.fetchArticleById = (article_id) => {
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          message: "article does not exist",
+        });
+      }
+      return rows[0];
+    });
+};
